e3f77e62aebd3ec95714a5e0b0078ec62c17435c
nick-fournier <nichfournier@gmail.com>
2022-05-09 16:27:34 -0700

    exe file created

--- firm_splitter.py
+++ firm_splitter.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 
 def firm_splitter():
+    print("Welcome to firm splitter! Let's go!!!")
+
     # Get settings from txt file
     with open('firm_splitter_settings.txt', 'r') as f:
         settings = dict(i.strip().split(':', 1) for i in f)
@@ -14,7 +16,10 @@ def firm_splitter():
         settings['separate_files'] = bool(settings['separate_files'].lower())
 
     # Load workbook
-    workbook = pd.read_excel(settings['input_file'], sheet_name=settings.get('input_sheet'))
+    try:
+        workbook = pd.read_excel(settings['input_file'], sheet_name=settings.get('input_sheet'))
+    except:
+        print("Couldn't find the input file specified :(")
 
     # If sheet not specified and only contains one sheet, turn into data frame
     if len(workbook) == 1 and isinstance(workbook, dict):
@@ -24,15 +29,25 @@ def firm_splitter():
     if not os.path.exists(settings['output_folder']):
         os.mkdir(settings['output_folder'])
 
+    # Check if split column exists
+    if settings.get('split_column') not in workbook.columns:
+        print('Split columns: '  + settings.get('split_column') + ' not found in the sheet!')
+        raise
+
+
     # Split the data and save
     if settings['separate_files']:
         for name, firm_df in workbook.groupby(settings.get('split_column')):
             firm_df.to_excel(os.path.join(settings['output_folder'], name + '.xlsx'))
+            print('Just split: ' + name)
     else:
         fname = os.path.join(settings['output_folder'], os.path.splitext(settings['input_file'])[0] + '_split.xlsx')
         with pd.ExcelWriter(fname) as writer:
             for name, firm_df in workbook.groupby(settings.get('split_column')):
                 firm_df.to_excel(writer, sheet_name=name)
+                print('Just split: ' + name)
+
+    print("DONE!!! Hooray!")
 
 
 if __name__ == "__main__":
